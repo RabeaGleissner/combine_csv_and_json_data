afb3a32c33f32420826d9f10555bb9fe6df17f85
Rabea Gleissner <rabeagleissner@gmail.com>
2020-11-29 13:33:01 +0800

    Use class method instead

--- lib/args_parser.rb
+++ lib/args_parser.rb
@@ -1,8 +1,8 @@
 class ArgsParser
-  def parse(arguments)
+  def self.parse(arguments)
     format_flag_index = arguments.find_index('--format')
     format = arguments[format_flag_index + 1]
     files = arguments.reject.with_index{|x, i| (i == format_flag_index || i == format_flag_index + 1 )}
-    { format: format, files: files}
+    {format: format, files: files}
   end
 end
--- spec/args_parser_spec.rb
+++ spec/args_parser_spec.rb
@@ -1,17 +1,15 @@
 require 'args_parser'
 
 describe ArgsParser do
-  let(:args_parser) {ArgsParser.new}
-
   it 'identifies target format and file names with format at the beginning' do
     given_arguments = ['--format', 'json', 'journals.csv', 'articles.csv', 'authors.csv']
 
-    expect(args_parser.parse(given_arguments)).to eq({ format: 'json', files: ['journals.csv', 'articles.csv', 'authors.csv']})
+    expect(ArgsParser.parse(given_arguments)).to eq({ format: 'json', files: ['journals.csv', 'articles.csv', 'authors.csv']})
   end
 
   it 'identifies target format and file names with format at the end' do
     given_arguments = ['journals.csv', 'articles.csv', 'authors.csv', '--format', 'json']
 
-    expect(args_parser.parse(given_arguments)).to eq({ format: 'json', files: ['journals.csv', 'articles.csv', 'authors.csv']})
+    expect(ArgsParser.parse(given_arguments)).to eq({ format: 'json', files: ['journals.csv', 'articles.csv', 'authors.csv']})
   end
 end
